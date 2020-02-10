a2098a8a6de59c576bcc10e08859e52714ab9834
Genny Norman <gnorman@bu.edu>
2020-02-10 12:39:02 -0500

    placate flake

--- test_api.py
+++ test_api.py
@@ -8,7 +8,7 @@ def test_fetch_coords_name_good():
     assert  res[0] == "45.62779998779297", res[1] == "-122.40399932861328"
 
 def test_fetch_coords_ident_bad():
-    assert fetchLatLongFromIdent(02348) == "ERROR: coordinates not found"
+    assert fetchLatLongFromIdent(2348) == "ERROR: coordinates not found"
 
 def test_fetch_coords_ident_good():
     res = fetchLatLongFromIdent("00A")
